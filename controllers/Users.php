be8562fbd479e73b4a5243bd33ecf3ac31cedafb
Lyubov Zhurba <l.zhurba@lucky-labs.com>
2017-08-02 16:53:44 +0300

    <lesson4> login method was added in controller

--- controllers/Users.php
+++ controllers/Users.php
@@ -11,6 +11,14 @@ class Users extends Controller
 
     public function admin_login()
     {
-        
+        if ( isset($_POST['login']) && !empty($_POST['login']) && !empty($_POST['pass']) ) {
+            $user = $this->model->getUserByLoin($_POST['login']);
+            $pass = md5(Config::get('pass_salt') . $_POST['pass'] . Config::get('pass_pep'));
+            if ($user && $user['is_active'] && $user['password'] == $pass) {
+                Session::setValue('user' , $user['login']);
+                Session::setValue('role' , $user['role']);
+            }
+            Router::redirect('admin');
+        }
     }
 }
